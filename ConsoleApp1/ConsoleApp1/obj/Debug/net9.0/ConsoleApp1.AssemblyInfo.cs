67c57abc55807fb001fe0452179fceb4395b7a0b
wanjiru-esther <septemberwanjiru@gmail.com>
2025-02-28 01:18:16 -0500

    Project A: Implementation of Phase 1: Basic Thread Operations with Cookie System - Created threads to perform concurrent operations for adding and taking cookies from a shared jar. - Demonstrated basic thread creation and management using `Thread` class. - Ensured thread safety by using `lock` to synchronize access to shared resource (cookie jar). - Implemented a simple Cookie Jar system with methods for adding and taking cookies. - Phase 1 successfully completes basic thread operations with proper synchronization.

--- ConsoleApp1/ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f49777542f9de68659b11cf4bad899e94ad3cd20")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b0871f643f683a8c24b14817a22847c43681841")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
